0fd991c90b618739470cd82fdfead9e6ee61cb82
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-10-24 20:35:34 +0530

    bloggy added

--- components/work/Data.jsx
+++ components/work/Data.jsx
@@ -1,5 +1,5 @@
 import Work1 from '../../src/assets/expense.png';
-import Work3 from '../../src/assets/movieflicker.png';
+import Work2 from '../../src/assets/movieflicker.png';
 import Work4 from '../../src/assets/campusc.jpg';
 import Work5 from '../../src/assets/productpulse.png';
 import Work6 from '../../src/assets/pickverse.png';
@@ -7,10 +7,47 @@ import Work7 from '../../src/assets/pollapp.png';
 import Work8 from '../../src/assets/hero.png';
 import Work9 from '../../src/assets/quizopia.jpg';
 import Work10 from '../../src/assets/Fizzi.jpg';
+import Work11 from '../../src/assets/bloggy.png';
 
 export const projectsData = [
 	{
 		id: 1,
+		image: Work8,
+		title: 'BuyIt',
+		category: 'web',
+		sourceUrl: 'https://github.com/Saurav-KumarSingh/BuyIt',
+		status: 'completed',
+		description: 'BuyIt is a MERN-based e-commerce platform with secure auth, payments, product management, and cloud image handling for users and admins.',
+	},
+	{
+		id: 2,
+		image: Work9,
+		title: 'Quizopia',
+		category: 'app',
+		sourceUrl: 'https://github.com/Saurav-KumarSingh/Quizopia',
+		status: 'completed',
+		description: 'Quizopia is a modern, interactive mobile quiz application built with Flutter and Firebase. Challenge your knowledge across various categories, compete on leaderboards, and track your progress in real-time!',
+	},
+	{
+		id: 3,
+		image: Work11,
+		title: 'Bloggy',
+		category: 'web',
+		sourceUrl: 'https://github.com/Saurav-KumarSingh/Bloggy',
+		status: 'completed',
+		description: 'A simple social post app where users can create, edit, delete posts, and comment on them.'
+	},
+	{
+		id: 4,
+		image: Work10,
+		title: 'Fizzi',
+		category: 'app',
+		sourceUrl: 'https://github.com/Saurav-KumarSingh/Fizzi',
+		status: 'completed',
+		description: 'Fizzi is a modern Flutter-based social media app designed for seamless interaction, content sharing, and community engagement.',
+	},
+	{
+		id: 5,
 		image: Work1,
 		title: 'Expense Share',
 		category: 'web',
@@ -19,8 +56,8 @@ export const projectsData = [
 		description: 'A MERN stack application for tracking and managing personal expenses. This user-friendly application allows individuals to create, update, and delete their expense records.',
 	},
 	{
-		id: 2,
-		image: Work3,
+		id: 6,
+		image: Work2,
 		title: 'MovieFlicker',
 		category: 'web',
 		sourceUrl: 'https://github.com/Saurav-KumarSingh/Analyse-movie',
@@ -28,7 +65,7 @@ export const projectsData = [
 		description: 'Discover your favorite movies and web series with MovieFlicker! Get in-depth details like ratings, cast, synopsis, and more—all in one place.',
 	},
 	{
-		id: 3,
+		id: 7,
 		image: Work4,
 		title: 'Campus Connect',
 		category: 'app',
@@ -37,7 +74,7 @@ export const projectsData = [
 		description: 'Building a thriving alumni network is vital for fostering relationships, sharing knowledge, and creating opportunities for growth.',
 	},
 	{
-		id: 4,
+		id: 8,
 		image: Work5,
 		title: 'Product Pulse',
 		category: 'app',
@@ -46,7 +83,7 @@ export const projectsData = [
 		description: 'Product Pulse is a mobile application designed to help consumers track the expiry dates of their products, reducing waste and promoting smarter shopping habits.',
 	},
 	{
-		id: 5,
+		id: 9,
 		image: Work6,
 		title: 'PickVerse',
 		category: 'app',
@@ -56,7 +93,7 @@ export const projectsData = [
 	},
 	
 	{
-		id: 6,
+		id: 10,
 		image: Work7,
 		title: 'PollApp',
 		category: 'app',
@@ -64,33 +101,7 @@ export const projectsData = [
 		status: 'completed',
 		description: 'PollApp is a Flutter application that allows users to create polls and gather suggestions from others. It provides an interactive and engaging way to collect opinions and make informed decisions.',
 	},
-	{
-		id: 7,
-		image: Work8,
-		title: 'BuyIt',
-		category: 'web',
-		sourceUrl: 'https://github.com/Saurav-KumarSingh/BuyIt',
-		status: 'completed',
-		description: 'BuyIt is a MERN-based e-commerce platform with secure auth, payments, product management, and cloud image handling for users and admins.',
-	},
-	{
-		id: 8,
-		image: Work9,
-		title: 'Quizopia',
-		category: 'app',
-		sourceUrl: 'https://github.com/Saurav-KumarSingh/Quizopia',
-		status: 'completed',
-		description: 'Quizopia is a modern, interactive mobile quiz application built with Flutter and Firebase. Challenge your knowledge across various categories, compete on leaderboards, and track your progress in real-time!',
-	},
-	{
-		id: 9,
-		image: Work10,
-		title: 'Fizzi',
-		category: 'app',
-		sourceUrl: 'https://github.com/Saurav-KumarSingh/Fizzi',
-		status: 'completed',
-		description: 'Fizzi is a modern Flutter-based social media app designed for seamless interaction, content sharing, and community engagement.',
-	},
+	
 ];
 
 export const projectsNav = [
